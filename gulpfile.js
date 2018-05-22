7bd8bc7de2171de46a1504579e6e47d4724639ed
Diego Bernal <diego91bernal@gmail.com>
2018-05-21 18:39:15 -0700

    fix: add proper error handling to gulp inject

--- gulpfile.js
+++ gulpfile.js
@@ -75,21 +75,25 @@ gulp.task("inject", function() {
     return new Promise(function(resolve, reject) {
         readYaml("_config.yml", function(err, data) {
             if (err) {
-                reject(err);
+                return reject(err);
             }
             resolve(data.url);
         });
-    }).then(function(url) {
-        var vendorsStream = gulp.src(["dist/vendors.*"], { read: false });
-        var siteStream = gulp.src(["dist/site.*"], { read: false });
-        return gulp
-            .src("dist/*.html")
-            .pipe(
-                inject(series(vendorsStream, siteStream), {
-                    relative: true,
-                    addPrefix: url
-                })
-            )
-            .pipe(gulp.dest("dist"));
-    });
+    })
+        .then(function(url) {
+            var vendorsStream = gulp.src(["dist/vendors.*"], { read: false });
+            var siteStream = gulp.src(["dist/site.*"], { read: false });
+            return gulp
+                .src("dist/*.html")
+                .pipe(
+                    inject(series(vendorsStream, siteStream), {
+                        relative: true,
+                        addPrefix: url
+                    })
+                )
+                .pipe(gulp.dest("dist"));
+        })
+        .catch(function(err) {
+            throw new Error(err);
+        });
 });
