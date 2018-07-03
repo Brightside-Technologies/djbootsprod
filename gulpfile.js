cf39c456bd47e751e7b64c5fb870f59d315e1012
Diego Bernal <diego91bernal@gmail.com>
2018-07-02 19:22:14 -0700

    feature: image-set

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var cleanCss = require("gulp-clean-css");
 var autoprefixer = require("gulp-autoprefixer");
 var htmlmin = require("gulp-htmlmin");
 var inject = require("gulp-inject");
+var responsive = require("gulp-responsive");
 var del = require("del");
 var readYaml = require("read-yaml");
 var series = require("stream-series");
@@ -96,4 +97,55 @@ gulp.task("inject", function() {
         .catch(function(err) {
             throw new Error(err);
         });
+});
+
+gulp.task("responsive-images", function() {
+    return gulp
+        .src("src/jekyll/assets/images/*.{jpg,png}")
+        .pipe(
+            responsive(
+                {
+                    // Convert all images to JPEG format
+                    "*": [
+                        {
+                            // image-medium.jpg is 375 pixels wide
+                            width: 375,
+                            rename: {
+                                suffix: "-md",
+                                extname: ".jpg"
+                            }
+                        },
+                        {
+                            // image-large.jpg is 480 pixels wide
+                            width: 480,
+                            rename: {
+                                suffix: "-lg",
+                                extname: ".jpg"
+                            }
+                        },
+                        {
+                            // image-extralarge.jpg is 768 pixels wide
+                            width: 768,
+                            rename: {
+                                suffix: "-xl",
+                                extname: ".jpg"
+                            },
+                            skipOnEnlargement: true
+                        }
+                    ]
+                },
+                {
+                    // Global configuration for all images
+                    // The output quality for JPEG, WebP and TIFF output formats
+                    quality: 80,
+                    // Use progressive (interlace) scan for JPEG and PNG output
+                    progressive: true,
+                    // Strip all metadata
+                    withMetadata: false,
+                    // Do not emit the error when image is enlarged.
+                    errorOnEnlargement: false
+                }
+            )
+        )
+        .pipe(gulp.dest("src/jekyll/assets/responsive-images"));
 });
